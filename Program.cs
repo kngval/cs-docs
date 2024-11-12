b5432d38deafd657d00b55bdc797bbefb9c7ade1
kngval <bkvalteocs@gmail.com>
2024-11-12 21:24:35 +0800

    records

--- Program.cs
+++ Program.cs
@@ -6,8 +6,10 @@ public class Program {
     
     // ValueTypes.MutableTest(); // Struct value type test
     // EnumType.DisplayPlanet(); // Enum value type test
-    Tuple.CreateTuple(); // Tuple type test
-    Console.WriteLine(Tuple.ReturnTuple(new int[]{-9,-123,12,0,30,1121})); // tuple return type
+    // Tuple.CreateTuple(); // Tuple type test
+    // Console.WriteLine(Tuple.ReturnTuple(new int[]{-9,-123,12,0,30,1121})); // tuple return type
+    
+    Records.DisplayPerson();
   }
 
 
